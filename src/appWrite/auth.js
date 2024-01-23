7547cd6ff98f5c5753a5a2e5ecc6a54806c08e58
Nazam-Kalsi <nzamkalsi@gmail.com>
2024-01-23 14:33:14 +0530

    initial commit

---
+++ src/appWrite/auth.js
@@ -0,0 +1,60 @@
+import { Client, Account, ID } from "appwrite";
+import config from "../ConfigEnv/Config";
+
+export class authService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client.setEndpoint(config.appWriteID).setProject(config.projectID);
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      let accountCreation = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (accountCreation) {
+        console.log("userAccount : ", accountCreation);
+        // calllogin
+        return this.login(email, password);
+      } else {
+        return accountCreation;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      let user = await this.account.createEmailSession(email, password);
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+  async currentUser(){
+    try {
+        const currentUser=await this.account.getCurrent();
+        return currentUser;        
+    } catch (error) {
+        throw error;
+    }
+    return null;
+  }
+  async logOut(){
+    try {
+        await this.account.deleteSessions();
+    } catch (error) {
+        throw error;
+    }
+  }
+}
+
+const authservice = new authService();
+export default authservice;
---
+++ src/appWrite/bucketService.js
@@ -0,0 +1,119 @@
+import config from "../ConfigEnv/Config";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class dbService {
+  client = new client();
+  databases;
+  Storage;
+
+  constructor() {
+    this.client.setEndpoint(config.appWriteID).setProject(config.projectID);
+    this.databases = new Databases(this.client);
+    this.Storage = new Storage(this.client);
+  }
+  async createPost({ title, slug, content, featuredImage, status, userID }) {
+    try {
+      const items = await this.databases.createDocument(
+        config.databaseID,
+        config.collectionID,
+        slug,
+        {
+          title,
+          content,
+          status,
+          userID,
+          featuredImage,
+        }
+      );
+      return items;
+    } catch (error) {
+        throw new Error("could not create the blog");
+    }
+  }
+
+  async updateBlog(slug, { title, featuredImage, content, status }) {
+    try {
+      let updatedBlog = await this.databases.updateDocument(
+        config.databaseID,
+        config.collectionID,
+        slug,
+        {
+          title,
+          content,
+          status,
+          featuredImage,
+        }
+      );
+      return updatedBlog;
+    } catch (error) {
+      throw new Error("could not update the blog");
+    }
+  }
+
+  async deleteBlog(slug){
+    try{
+
+        let deleteBlog =await this.databases.deleteDocument(config.databaseID,config.collectionID,slug);
+        return deleteBlog;
+    }
+    catch{
+        throw new Error("Could not delete the blog");
+    }
+  }
+
+  async getBlog(slug){
+    try{
+
+        let singleBlog =await this.databases.getDocument(config.databaseID,config.collectionID,slug);
+        return singleBlog;
+    }
+    catch{
+        throw new Error("Could not parse the blog");
+    }
+  }
+
+  async allBlogs(qur=['status','active']){
+    try{
+
+        let allBlogs=this.databases.listDocuments(config.databaseID,config.collectionID,que)
+        return allBlogs;
+    }
+    catch{
+        throw new Error("could not get all blogs.Try again!")
+    }
+  }
+
+
+  async uploadImg(file){
+    try {
+        return await this.Storage.createFile(config.bucketID,ID.unique(),file)
+        
+    } catch (error) {
+        throw new Error("could not upload file.Try again!")
+
+    }
+  }
+
+  async deleteImg(fileID){
+    try {
+        await this.Storage.deleteFile(config.bucketID,fileID)
+        return true
+    } catch (error) {
+        throw new Error("could not delete file.Try again!")
+        return false            
+    }
+
+  }
+
+   preview(fileID){
+    try {
+        return this.Storage.getFilePreview(config.bucketID,fileID)
+    } catch (error) {
+        
+    }
+  }
+
+}
+
+const dbservice = new dbService();
+export default dbservice;
