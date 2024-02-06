dda61ffdbba1c360988e89534b814e5018f1ac9d
Nazam-Kalsi <nzamkalsi@gmail.com>
2024-02-06 17:05:41 +0530

    minor changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,13 +8,13 @@ function Card({
   return (
 
     <Link to={`/post/${$id}`}>
-      <div className='py-4 px-8 min-w-80 border border-gray-500 rounded-xl hover:bg-black hover:scale-[1.01] transition-all'>
+      <div className='py-4 px-8 mx-auto min-w-80 border border-gray-500 rounded-xl hover:bg-black hover:scale-[1.01] transition-all'>
         <div className=' overflow-hidden bh-64 w-5/6 '>
           <img src={dbService.preview(featuredImage)} alt={Title} className='w-5/6  rounded-xl size-40 '/> 
            {/*we store id of image in db */}
         </div>
         <div className='flex-1'>
-        <h2 className='text-xl font-semibold text-center py-2 px-4 mb-4'>{Title}</h2>
+        <h2 className='text-xl font-semibold py-2 mb-4'>{Title}</h2>
         </div>
       </div>
       </Link>
---
+++ src/pages/Modal.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Modal({}) {
+  return (
+<dialog >
+  <p className=''>Processing...</p>
+</dialog>  )
+}
+
+export default Modal
